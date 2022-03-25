d0e6cad30a86c675ba5820243f1cf3263d7cbf54
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-03-25 01:32:20 +0100

    mt6768-common: fingerprint: Move fingerprint HIDL to 2.3
    Change-Id: I2e5ed64ac7ae2f1003503072666204c0788a003a

--- mt6768.mk
+++ mt6768.mk
@@ -54,7 +54,7 @@ PRODUCT_COPY_FILES += \
 
 # Biometrics
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.mt6768
+    android.hardware.biometrics.fingerprint@2.3-service.mt6768
 
 PRODUCT_PACKAGES += \
     libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
