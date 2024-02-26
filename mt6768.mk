5c89002e9045d9535713e58bc1fd9c7db5896df5
cyberknight777 <cyberknight755@gmail.com>
2024-02-26 07:45:58 +0530

    mt6768-common: Use FUSE passthrough by default
     - The prop is supposed to be set in product instead of system as per [1].
    
     [1]: https://source.android.com/docs/core/storage/fuse-passthrough
    
    Change-Id: I4c467da6b1a659625aa20e79a718736b2ee9f915
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- mt6768.mk
+++ mt6768.mk
@@ -140,6 +140,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     FMRadio
 
+# FUSE Passthrough
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.fuse.passthrough.enable=true
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
